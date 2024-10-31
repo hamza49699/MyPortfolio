da2507398499efcc5db201a2e9b0fc4416118f21
Hamza Javed <hamzakhan.hk1994@gmail.com>
2024-10-31 05:18:12 +0500

    updated

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,24 +1,28 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from 'react';
+import Navbar from './Navbar';
 
 const Hero = () => {
   return (
-    <div id="hero" className='min-h-screen bg-no-repeat bg-[url(/profile1.jpeg)]  bg-cover m-10' style={{backgroundSize: "35%" , backgroundPosition: "left 100px"}}>
-        
-     <Navbar />
-     <div className='container grid lg:grid-cols-2 h-[calc(100vh-60px)]'>
-  <div className='lg:block'></div>
-  <div className=' text-[80px] sm:text-[100px] font-bold leading-tight flex- justify-center flex items-center'>
-    <div> 
-      <p>HELLO</p>
-      <p>THIS IS</p>
-      <p>HAMZA</p>
-      <p>JAVED</p>
+    <div
+      id="hero"
+      className='min-h-screen bg-no-repeat bg-[url(/profile1.jpeg)] bg-cover m-10'
+      style={{ backgroundSize: "35%", backgroundPosition: "left 100px" }}
+    >
+      <Navbar />
+      <div className='container grid lg:grid-cols-2 h-[calc(100vh-60px)]'>
+        <div className='lg:block'></div>
+        <div className='flex justify-center items-center text-[80px] sm:text-[80px] font-bold leading-tight p-4 md:p-10'>
+          <div>
+            
+            <p>THIS IS</p>
+            <p>HAMZA</p>
+            <p>JAVED</p>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-    </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
+
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,43 @@
-import React from 'react'
+"use client"; // Mark this component as a Client Component
+
+import React, { useState } from 'react';
 import { TfiMenuAlt } from "react-icons/tfi";
 
 const Navbar = () => {
-  return (
-    <div className='container pt-2 bg-yellow-400 pb-2  '>
-        <div className='flex justify-between items-center'>
-            <div className='text-xl font-extrabold'>Muhammad Hamza Javed</div>
+  const [isOpen, setIsOpen] = useState(false); // State to manage menu visibility
 
-            <ul className='gap-10 lg:gap-16 hidden md:flex font-bold text-2xl '>
+  const toggleMenu = () => {
+    setIsOpen(!isOpen); // Toggle the menu
+  };
+
+  return (
+    <div className='container pt-2 bg-yellow-400 pb-2'>
+      <div className='flex justify-between items-center'>
+        <div className='text-xl font-extrabold'>Muhammad Hamza Javed</div>
+
+        <ul className='gap-10 lg:gap-16 hidden md:flex font-bold text-2xl'>
+          <li className='menuLink'><a href='#hero'>Home</a></li>
+          <li className='menuLink'><a href='#about'>About</a></li>
+          <li className='menuLink'><a href='#contact'>Contact</a></li>
+          <li className='menuLink'><a href='#skills'>Skills</a></li>
+        </ul>
+
+        <TfiMenuAlt className='md:hidden cursor-pointer' size={30} onClick={toggleMenu} />
+      </div>
+
+      {/* Dropdown menu for small screens */}
+      {isOpen && (
+        <div className='md:hidden bg-yellow-400 mt-2'>
+          <ul className='flex flex-col font-bold text-2xl'>
             <li className='menuLink'><a href='#hero'>Home</a></li>
             <li className='menuLink'><a href='#about'>About</a></li>
             <li className='menuLink'><a href='#contact'>Contact</a></li>
             <li className='menuLink'><a href='#skills'>Skills</a></li>
-            </ul>
-            <TfiMenuAlt className='md:hidden' size={30} />
+          </ul>
         </div>
-      
+      )}
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
